2aaa05df07c2c81e4ea66e399c93152d9aef5bad
Telini07 <brunotelinirocha50@gmail.com>
2023-05-02 09:34:52 -0300

    0205

---
+++ banco/banco.sql
@@ -0,0 +1,16 @@
+ -- Arquivo de criação do banco de dados
+
+
+ DROP DATABASE IF EXISTS projeto1;
+ CREATE DATABASE projeto1;
+ USE projeto1;
+ 
+ CREATE TABLE usuario 
+ (
+	id_usuario int (4) NOT NULL PRIMARY KEY auto_increment,
+    nome_usuario varchar (50) NOT NULL,
+    email_usuario varchar (50) NOT NULL,
+    fone_usuario varchar (30) NULL,
+    senha varchar (30) NOT NULL
+ );
+
---
+++ banco/revisao.sql
@@ -0,0 +1,16 @@
+  -- COMANDOS SQL SE DIVIDEM EM DUAS LINGUAGENS
+  -- --------------------------------------------------
+  -- DDL (Data Definition Language)
+  -- => Comandos de definição do banco de dados
+  -- Exemplos: CREATE DATABASE NAME
+  --			  DROP DATABASE NAME
+  --			  CREATE TABLE NAME (COLUNAS) 
+  -- 		  DROP TABLE NAME 
+  -- --------------------------------------------------
+  -- DML (Data Manipulation Language)
+  -- => Comandos de manipulação de dados
+  -- Exemplos: SELECT COLUNAS FROM TABELA
+  --		   INSERT INTO TABELA (COLUNAS) VALUES (VALORES)
+  --		   DELETE FROM TABELA
+  --  		   UPDATE COLUNAS FROM TABELA
+  
